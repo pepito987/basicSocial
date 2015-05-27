9fd31ab39e8dd48434992ab00a31f8e5bf84919b
giuseppe <giuseppe@r3d.co.uk>
2015-05-27 16:45:07 +0100

    execRequestShouldReturnTheUserWallIfTheCommandIsWallEndThereAreMoreThenOneSubscriber

--- src/main/basicSocial/social/Social.java
+++ src/main/basicSocial/social/Social.java
@@ -10,20 +10,25 @@ import java.util.Map;
 public class Social {
 	
 	public Map<String,List<String>> messages = new HashMap<String, List<String>>();
-	public List<String> followers = new ArrayList<String>();
+	public Map<String,List<String>> followers = new HashMap<String, List<String>>();
+
 
 	public List<String> execRequest(String cmd) {
 		String[] cmd_line = cmd.split("\\s{1}",3);
-				
+	
 		if(cmd_line.length >1 ){
 			switch (cmd_line[1]) {
 			case "follow":{
-				this.followers.add(cmd_line[2]);
-				return followers;
+				if( this.followers.get(cmd_line[0]) == null){
+					this.followers.put(cmd_line[0], new ArrayList<String>());
+				}
+				this.followers.get(cmd_line[0]).add(cmd_line[2]);
+				return followers.get(cmd_line[0]);
 			} 
 			case "wall":{
 				List<String> wall = this.messages.get(cmd_line[0]);
-				for (String user : followers) {
+				List<String> follow = this.followers.get(cmd_line[0]);
+				for (String user : follow) {
 					wall.addAll(messages.get(user));
 				}
 				return wall;
