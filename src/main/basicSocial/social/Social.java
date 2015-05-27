10d11db1f5d4b1613c153bf737e00bc80cef3943
giuseppe <giuseppe@r3d.co.uk>
2015-05-27 14:48:55 +0100

    execRequestShouldAddTheUserToTheFollowListIfTheCommandIsFollow

--- src/main/basicSocial/social/Social.java
+++ src/main/basicSocial/social/Social.java
@@ -13,12 +13,15 @@ public class Social {
 	public List<String> followers = new ArrayList<String>();
 
 	public List<String> execRequest(String cmd) {
-		String[] cmd_line = cmd.split(" -> ");
+		String[] cmd_line = cmd.split("\\s{1}",3);
 		if(cmd_line.length >1 ){
+			if( cmd_line[1].equals("follow")){
+				this.followers.add(cmd_line[2]);
+			}
 			if( this.messages.get(cmd_line[0]) != null)
 				this.messages.get(cmd_line[0]).add(cmd_line[1]);
 			else{
-				this.messages.put(cmd_line[0].trim(), new ArrayList<String>(Arrays.asList(cmd_line[1])));
+				this.messages.put(cmd_line[0].trim(), new ArrayList<String>(Arrays.asList(cmd_line[2])));
 			}	
 		}
 		return this.messages.get(cmd_line[0]);
